a4c3328484165e0e5ee995b7f36fff489906f1f4
yrd <yash.deorah@somaiya.edu>
2020-06-26 18:28:28 +0530

    hopefully last

--- homecook/settings.py
+++ homecook/settings.py
@@ -79,11 +79,6 @@ WSGI_APPLICATION = 'homecook.wsgi.application'
 
 DATABASES = {
     'default': {
-        ''' 'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'homecook',
-        'USER':'postgres',
-        'PASSWORD':'postgres',
-        'HOST':'localhost' '''
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
